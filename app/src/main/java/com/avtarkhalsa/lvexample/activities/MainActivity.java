2fc7ab9290276679487f4ffab29d7405878411c1
akhalsa <akhalsa43@gmail.com>
2016-12-25 14:56:53 -0500

    put the activity into an activities package

--- app/src/main/java/com/avtarkhalsa/lvexample/activities/MainActivity.java
+++ app/src/main/java/com/avtarkhalsa/lvexample/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.avtarkhalsa.lvexample;
+package com.avtarkhalsa.lvexample.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.avtarkhalsa.lvexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
