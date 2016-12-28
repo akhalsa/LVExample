8b79163f4dd55d19570a4ccef38c67606c1839c5
akhalsa <akhalsa43@gmail.com>
2016-12-27 22:35:58 -0500

    unused import

--- app/src/main/java/com/avtarkhalsa/lvexample/activities/MainActivity.java
+++ app/src/main/java/com/avtarkhalsa/lvexample/activities/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
